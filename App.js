78949c2d777317ee615dde57781d09040fc5a507
Sang <hoang84119@gmail.com>
2018-07-16 13:27:25 +0700

    Fix

--- App.js
+++ App.js
@@ -1,25 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
   View,
-  FlatList
-} from 'react-native';
-import data from './data/json'
+  FlatList,
+  Image
+} from "react-native";
+import data from "./data/json";
+import HTML from 'react-native-render-html'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: true
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ refreshing: false });
+  }
+
   render() {
     return (
-      <FlatList/>
+      <FlatList
+        refreshing={this.state.refreshing}
+        onRefresh={this._refreshing}
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={myStyle.container}>
+            <Image source={{ uri: item.image.value }} style={myStyle.image} />
+            <FlatList
+              style={myStyle.item}
+              data={Object.values(item.info)}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) => (
+                // <Text style={myStyle.text}>
+                //   {item.name}: {item.value}
+                // </Text>
+                <HTML html={`${item.prepend} ${item.name}: ${item.value} ${item.append}`}/>
+              )}
+            />
+          </View>
+        )}
+      />
     );
   }
+
+  _refreshing = () => {
+    //this.setState({ refreshing: false });
+  };
 }
 
-const styles = StyleSheet.create({
+const myStyle = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    borderColor: "#000",
+    margin: 10,
+    borderWidth: 1,
+    flexDirection: "row"
   },
+  image: { marginVertical: 5, width: 150, height: 150 },
+  item:{ margin: 5, flexDirection: "column" },
+  text: {
+    color: "#808080",
+    fontSize: 16,
+    marginBottom: 5
+  }
 });
--- data/json.js
+++ data/json.js
@@ -2,123 +2,182 @@ export default (lstproduct = [
   {
     image: {
       name: "Hình ảnh",
-      value: "http://abc.com/1.png"
+      value:
+        "https://cdn4.tgdd.vn/Products/Images/42/168999/samsung-galaxy-s9-plus-128gb-vang-dong-90ngay-600x600.jpg"
       //'prepend':'',
       //'append':'',
     },
-    id: {
-      name: "Mã SP",
-      value: "238"
-      //'prepend':'',
-      //'append':'',
-    },
-    price: {
-      name: "Giá tiền",
-      value: "300000"
-    },
-    color: {
-      name: "Màu sắc",
-      value: "Đỏ, đen"
-    },
-    size: {
-      name: "Kích thước",
-      value: " 70 x 153"
+    info: {
+      id: {
+        name: "Mã SP",
+        value: "238",
+        prepend:'<span style="color:red; font-weight:bold">',
+        append:'</span>',
+      },
+      name:{
+          name:"Tên sản phẩm",
+          value:"Galaxy",
+          prepend:'',
+          append:'',
+      },
+      price: {
+        name: "Giá tiền",
+        value: "300000",
+        prepend:'',
+        append:'',
+      },
+      color: {
+        name: "Màu sắc",
+        value: "Đỏ, đen",
+        prepend:'',
+        append:'',
+      },
+      size: {
+        name: "Kích thước",
+        value: " 70 x 153",
+        prepend:'',
+        append:'',
+      },
     }
   },
   {
     image: {
       name: "Hình ảnh",
-      value: "http://abc.com/2.png"
-      //'prepend':'',
-      //'append':'',
+      value:
+        "https://cdn1.tgdd.vn/Products/Images/42/106211/samsung-galaxy-note8-90ngay-600x600.jpg",
+        prepend:'',
+        append:'',
     },
-    id: {
-      name: "Mã SP",
-      value: "239"
-    },
-    price: {
-      name: "Giá tiền",
-      value: "400000"
-    },
-    color: {
-      name: "Màu sắc",
-      value: "Đỏ, đen,trắng"
-    },
-    size: {
-      name: "Kích thước",
-      value: " 70 x 150"
+    info: {
+      id: {
+        name: "Mã SP",
+        value: "239",
+        prepend:'',
+        append:'',
+      },
+      price: {
+        name: "Giá tiền",
+        value: "400000",
+        prepend:'',
+        append:'',
+      },
+      color: {
+        name: "Màu sắc",
+        value: "Đỏ, đen,trắng",
+        prepend:'',
+        append:'',
+      },
+      size: {
+        name: "Kích thước",
+        value: " 70 x 150",
+        prepend:'',
+        append:'',
+      }
     }
   },
   {
     image: {
       name: "Hình ảnh",
-      value: "http://abc.com/3.png"
-      //'prepend':'',
-      //'append':'',
+      value:
+        "https://cdn2.tgdd.vn/Products/Images/42/166247/samsung-galaxy-a8-star-90ngay-600x600.jpg",
+        prepend:'',
+        append:'',
     },
-    id: {
-      name: "Mã SP",
-      value: "240"
-    },
-    price: {
-      name: "Giá tiền",
-      value: "300000"
-    },
-    color: {
-      name: "Màu sắc",
-      value: "Đỏ, đen"
-    },
-    size: {
-      name: "Kích thước",
-      value: " 70 x 153"
+    info: {
+      id: {
+        name: "Mã SP",
+        value: "240",
+        prepend:'',
+        append:'',
+      },
+      price: {
+        name: "Giá tiền",
+        value: "300000",
+        prepend:'',
+        append:'',
+      },
+      color: {
+        name: "Màu sắc",
+        value: "Đỏ, đen",
+        prepend:'',
+        append:'',
+      },
+      size: {
+        name: "Kích thước",
+        value: " 70 x 153",
+        prepend:'',
+        append:'',
+      }
     }
   },
   {
     image: {
       name: "Hình ảnh",
-      value: "http://abc.com/4.png"
-      //'prepend':'',
-      //'append':'',
+      value:
+        "https://cdn.tgdd.vn/Products/Images/42/142465/samsung-galaxy-a8-plus-2018-90ngay-600x600.jpg",
+        prepend:'',
+        append:'',
     },
-    id: {
-      name: "Mã SP",
-      value: "241"
-    },
-    price: {
-      name: "Giá tiền",
-      value: "500000"
-    },
-    color: {
-      name: "Màu sắc",
-      value: "Đỏ, đen"
-    },
-    size: {
-      name: "Kích thước",
-      value: " 70 x 153"
+    info: {
+      id: {
+        name: "Mã SP",
+        value: "241",
+        prepend:'',
+        append:'',
+      },
+      price: {
+        name: "Giá tiền",
+        value: "500000",
+        prepend:'',
+        append:'',
+      },
+      color: {
+        name: "Màu sắc",
+        value: "Đỏ, đen",
+        prepend:'',
+        append:'',
+      },
+      size: {
+        name: "Kích thước",
+        value: " 70 x 153",
+        prepend:'',
+        append:'',
+      }
     }
   },
   {
     image: {
       name: "Hình ảnh",
-      value: "http://abc.com/5.png"
-      //'prepend':'',
-      //'append':'',
+      value:
+        "https://cdn.tgdd.vn/Products/Images/42/112970/samsung-galaxy-j7-plus-hh-600x600.jpg",
+        prepend:'',
+        append:'',
     },
-    id: {
-      name: "Mã SP",
-      value: "242"
-    },
-    price: {
-      name: "Giá tiền",
-      value: "320000"
-    },
-    color: {
-      name: "Màu sắc",
-      value: "Đỏ, đen"
-    },
-    size: {
-      name: "Kích thước",
-      value: " 70 x 153"
+    info: {
+      id: {
+        name: "Mã SP",
+        value: "242",
+        prepend:'',
+        append:'',
+      },
+      price: {
+        name: "Giá tiền",
+        value: "320000",
+        prepend:'',
+        append:'',
+      },
+      color: {
+        name: "Màu sắc",
+        value: "Đỏ, đen",
+        prepend:'',
+        append:'',
+      },
+      size: {
+        name: "Kích thước",
+        value: " 70 x 153",
+        prepend:'',
+        append:'',
+      }
     }
   }
 ]);
